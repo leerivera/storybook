d580aa26e30a402910a1c85ddc240f29d57be438
leerivera <leerivera80@gmail.com>
2022-09-07 13:20:53 -0400

    handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 //load config
@@ -11,11 +12,17 @@ connectDB()
 
 const app = express()
 
+//logging
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 
 
 }
+
+//handlebars
+
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 3000
 
