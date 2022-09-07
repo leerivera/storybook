b689d0d6300e3dc1cc5806d5450d2b8d785fabbe
leerivera <leerivera80@gmail.com>
2022-09-07 10:37:10 -0400

    db.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
-//load congif
+//load config
 
 dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server ${process.env.NODE_ENV} ${PORT}`))
