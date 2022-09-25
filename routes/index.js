8334cbe85053f43e0e5a31cfb9e5a9ca09aac93e
leerivera <leerivera80@gmail.com>
2022-09-25 19:40:26 -0400

    story route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //loggin/landing page
 router.get('/', ensureGuest,(req, res) => {
     res.render('login', { layout: 'login'})
@@ -9,10 +11,18 @@ router.get('/', ensureGuest,(req, res) => {
 
 
 //dashboard get/dashboard
-router.get('/dashboard', ensureAuth,(req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err){
+        console.error(err)
+
+    }
+    
 })
 
 
