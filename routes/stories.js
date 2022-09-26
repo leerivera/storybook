96b05db3fcc0fbd5fc1214241f7b9f8aec422fdf
leerivera <leerivera80@gmail.com>
2022-09-26 18:14:42 -0400

    post and route for add stories created

--- routes/stories.js
+++ routes/stories.js
@@ -11,4 +11,18 @@ router.get('/add', ensureAuth,(req, res) => {
 })
 
 
+// @desc process add form
+// @route POST/stories
+router.get('/add', ensureAuth, async (req, res) => {
+    try{
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+
+    } catch (err) {
+        console.log(err)
+        res.render('/error/500')
+    }
+})
+
 module.exports = router
